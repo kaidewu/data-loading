607f8022cd642c57c96df37d414a9008a9855384
Kaide Wu <kaidewu@outlook.es>
2024-01-28 16:51:11 +0100

    Adding path output/input

--- src/core/config.py
+++ src/core/config.py
@@ -3,7 +3,8 @@ from typing import List
 
 
 class Settings:
-    services: List[str] = []
+    output_result_path: str = path.join(path.dirname(path.dirname(__file__)), path.join("output", "results.sql"))
+    input_path: str = path.join(path.dirname(path.dirname(__file__)), "input")
 
 
 settings = Settings()
