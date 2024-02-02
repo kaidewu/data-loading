89841a30456cabf47a78a232a980346ecebac39a
Kaide Wu <kaidewu@outlook.es>
2024-02-03 00:05:30 +0100

    Include more excel paths

--- src/core/config.py
+++ src/core/config.py
@@ -1,10 +1,12 @@
 from os import path
-from typing import List
 
 
 class Settings:
-    output_result_path: str = path.join(path.dirname(path.dirname(__file__)), path.join("output", "results.sql"))
-    input_path: str = path.join(path.dirname(path.dirname(__file__)), "input")
+    output_result_path: str = path.abspath(path.join(path.dirname(path.dirname(__file__)), path.join("output", "results.sql")))
+    input_path: str = path.abspath(path.join(path.dirname(path.dirname(__file__)), "input"))
+    output_bbdd_path: str = path.abspath(path.join(path.dirname(path.dirname(__file__)), path.join("output", "bbdd.xlsx")))
+    bbddpre_excel: str = path.abspath(path.join(path.dirname(__file__), path.join("data_excel", "BBDDPREASCIRES.xlsx")))
+    bbddpro_excel: str = path.abspath(path.join(path.dirname(__file__), path.join("data_excel", "BBDDPROASCIRES.xlsx")))
 
 
 settings = Settings()
